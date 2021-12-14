f247a64b85e13c497a49c8999a9cae5b15f517e5
sparrow48 <abunasib48@gmail.com>
2021-12-14 11:03:15 +0600

    add layout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Layout from "./layout/Layout";
+
 function App() {
   return (
-    <div className="App">
-      <header>hello</header>
+    <div className="font-inter">
+      <Router>
+        <Layout>
+          <h1>Hello</h1>
+        </Layout>
+      </Router>
     </div>
   );
 }
