744865fb9eec214bddac1b28462c84e939ff4732
Sparrow48 <abunasib48@gmail.com>
2022-02-10 15:22:45 +0600

    update portfolio section

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Work from "./work/Work";
 import mNotes from "../assets/mNotes.png";
+import e_shop from "../assets/e_shop.png";
 import imageGallery from "../assets/image-gallery.png";
 import burger from "../assets/burger.png";
 import portfolio from "../assets/portfolio.png";
@@ -9,6 +10,23 @@ function Works() {
   return (
     <section id="portfolio" className="border-b-2 ">
       <div className="flex flex-wrap justify-center py-16 mx-auto md:py-36 md:max-w-6xl">
+        <div className="flex flex-col pb-20 my-5 space-y-2 rounded md:py-10 md:my-12 md:max-w-xl hover:shadow-2xl">
+          <Work
+            image={e_shop}
+            title="e-Shop"
+            para="A simple e-commerce web application."
+            live="https://e-shop-tawny.vercel.app/"
+            github="https://github.com/Sparrow48/e-shop"
+          />
+          <div>
+            <ul className="flex items-center px-6 space-x-5 ">
+              <li className="px-2 py-1 bg-gray-200 rounded">React</li>
+              <li className="px-2 py-1 bg-gray-200 rounded">Tailwind</li>
+              <li className="px-2 py-1 bg-gray-200 rounded">Varcel</li>
+              <li className="px-2 py-1 bg-gray-200 rounded">Firebase</li>
+            </ul>
+          </div>
+        </div>
         <div className="flex flex-col pb-20 my-5 space-y-2 rounded md:py-10 md:my-12 md:max-w-xl hover:shadow-2xl">
           <Work
             image={mNotes}
